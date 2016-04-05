bb98e52f0ee4dfce7c77762aa67dd45dabd8510b
bigpada <anta0101@hotmail.com>
2016-04-05 11:59:14 -0500

    prueba

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,6 +1,6 @@
 var global_settings = {
-	//urlCORS: 'http://192.168.20.9/ApiNotify',
-	urlCORS: 'http://localhost:54515/',
+	urlCORS: 'http://192.168.20.9/ApiNotify',
+	//urlCORS: 'http://localhost:54515/',
 	liveReload: 1000,
 	//urlSocket: 'http://192.168.20.9:3100/'
 	urlSocket: 'http://localhost:3100/'	
