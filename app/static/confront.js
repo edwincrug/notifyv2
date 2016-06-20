a6478556885e5cabb2b062b59e7d869bccb7336b
bigpada <anta0101@hotmail.com>
2016-06-20 10:32:50 -0500

    lqma 20062016
    se agrego agrupado 3

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,5 +1,5 @@
 var global_settings = {
-	urlCORS: 'http://192.168.20.9/ApiNotify',
+	urlCORS: 'http://192.168.20.41/ApiNotify',
 	liveReload: 1000,
-	urlSocket: 'http://192.168.20.9:3100/'
+	urlSocket: 'http://192.168.20.41:3100/'
 } 	
